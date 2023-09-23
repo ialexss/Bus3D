616649f857ddb166f9f4b63728e1d4240d1aae70
ialexss <alexandersanchezsalces@gmail.com>
2023-09-23 14:00:44 -0400

    exhibit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import {Center, Environment,OrbitControls,Text3D,Text } from '@react-three/drei'
+import {Center, Environment,OrbitControls,Text3D,Text, CameraControls } from '@react-three/drei'
 import { useControls } from 'leva'
 
 
@@ -9,22 +9,23 @@ import Celcius from "./components/Celcius"
 
 function App() {
 
-  const {color,numero, colorNumero} = useControls({
+  const {color,numero, colorNumero,exhibir} = useControls({
     color: '#000000',
     numero: "21",
-    colorNumero: '#ffffff'
+    colorNumero: '#ffffff',
+    exhibir: false
   })
-
+  
   return (
     <div style={{width:'100%',height:'100vh'}} >
-      <Canvas camera={{zoom:1, position:[5,5,5],fov:80,far:2000}} gl={{preserveDrawingBuffer:true}}>
+      <Canvas camera={{zoom:1, position:[5,5,5],fov:80,far:2000}} shadows gl={{preserveDrawingBuffer:true}}>
         <Suspense fallback={null}>
           <Center top>
-              <Text3D position={[-0.50,3.6, 5]}  font="Inter_ExtraBold.json" size={0.5} height={0.07} type='font'  >
+              {/* <Text3D position={[-0.50,3.6, 5]}  font="Inter_ExtraBold.json" size={0.5} height={0.07} type='font'  >
                 {numero}
                 <meshStandardMaterial color={colorNumero} toneMapped={false} />
-              </Text3D>
-            <Celcius color={color} />
+              </Text3D> */}
+            <Celcius rotation-y={2} color={color} numero={numero} colorNumero={colorNumero} exhibir={exhibir} />
           </Center>
           <Environment preset="city" background blur={1} />
           <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
--- src/components/Celcius.jsx
+++ src/components/Celcius.jsx
@@ -5,14 +5,28 @@ Files: celcius.gltf [9.49MB] > celcius-transformed.glb [210.96KB] (98%)
 */
 
 import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { useGLTF,Text3D } from '@react-three/drei'
 
-export default function Model({color,props}) {
+export default function Model({color,numero,exhibir,colorNumero,props}) {
+  const bus = useRef();
+
+  useFrame((_state,delta) => {
+    if(exhibir){
+      bus.current.rotation.y += delta * 1;
+    }
+    
+  })
+
   const { nodes, materials } = useGLTF('/celcius-transformed.glb')
   return (
-    <group {...props} dispose={null}>
+    <group {...props} dispose={null} ref={bus}>
+      <Text3D position={[-0.50,3.6, 5]}  font="Inter_ExtraBold.json" size={0.5} height={0.07} type='font'  >
+        {numero}
+        <meshStandardMaterial color={colorNumero} toneMapped={false} />
+      </Text3D>
       <mesh geometry={nodes.bodi.geometry} material={materials.Default_Material} material-color={color} />
-      <mesh geometry={nodes.ban.geometry} material={nodes.ban.material} />
+      <mesh geometry={nodes.ban.geometry} material={nodes.ban.material}  />
     </group>
   )
 }
