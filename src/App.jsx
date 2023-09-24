79cbaba7987766f2d9478a3ab95a0da2b61597dc
ialexss <alexandersanchezsalces@gmail.com>
2023-09-23 20:18:12 -0400

    nuevo micro con nueva funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
 import './App.css'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import {Center, Environment,OrbitControls,Text3D,Text, CameraControls } from '@react-three/drei'
+import {Center, Environment ,OrbitControls,Stage} from '@react-three/drei'
 import { useControls } from 'leva'
 
-
-import Celcius from "./components/Celcius"
+import Micro from "./components/Micro"
 
 function App() {
 
-  const {color,numero, colorNumero,exhibir} = useControls({
-    color: '#000000',
-    numero: "21",
-    colorNumero: '#ffffff',
-    exhibir: false
+  const {color,numero,avanzar,retroceder,linea,girar} = useControls({
+    color: '#e2d5d5',
+    numero: "40",
+    linea: '#00903F',
+    avanzar: false,
+    retroceder: false,
+    girar: {
+      value: 0,
+      min: -10,
+      max: 10,
+      step: 1,
+    },
   })
   
   return (
     <div style={{width:'100%',height:'100vh'}} >
-      <Canvas camera={{zoom:1, position:[5,5,5],fov:80,far:2000}} shadows gl={{preserveDrawingBuffer:true}}>
-        <Suspense fallback={null}>
-          <Center top>
-              {/* <Text3D position={[-0.50,3.6, 5]}  font="Inter_ExtraBold.json" size={0.5} height={0.07} type='font'  >
-                {numero}
-                <meshStandardMaterial color={colorNumero} toneMapped={false} />
-              </Text3D> */}
-            <Celcius rotation-y={2} color={color} numero={numero} colorNumero={colorNumero} exhibir={exhibir} />
-          </Center>
-          <Environment preset="city" background blur={1} />
-          <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
-        </Suspense>
+      <Canvas camera={{zoom:1, position:[5,2,5],fov:80,far:2000}} shadows gl={{preserveDrawingBuffer:true,}}>
+          <Suspense fallback={null}>
+            <Center top>
+              <Micro color={color} numero={numero} linea={linea} girar={girar} avanzar={avanzar} retroceder={retroceder} />
+            </Center>
+            <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2.1} />
+          </Suspense>
+          <Environment preset='sunset' background blur={0.5}/>
       </Canvas>
     </div>
   )
